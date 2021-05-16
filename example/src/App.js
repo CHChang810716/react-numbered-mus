67109ad5d84df5eaaf92a9189c44e5adaf3c8b2e
C.H. Chang <CHChang810716@gmail.com>
2021-05-16 11:41:27 +0800

    add num staff example

--- example/src/App.js
+++ example/src/App.js
@@ -1,103 +1,106 @@
-import React from 'react'
-import {NumStaffScore, Score} from 'react-numbered-mus'
-const measureStart = (keyTxt, nt, octave, optional) => {
-  return Object.assign(note(keyTxt, nt, octave, {
-    measureStart: true
-  }), optional)
-}
-const note = (keyTxt, nt, octave, optional) => {
-  return Object.assign({
-    keyTxt: keyTxt, 
-    noteType: nt, 
-    octave: octave
-  }, optional)
-}
+import React from 'react';
+import {NumStaffScore} from 'react-numbered-mus'
 const score = {
   notes: [
-    measureStart(1, 2, 0, {
-      slur: [0, 2],
-      baseTune: 'D',
-      tempoPerMeasure: 4,
-      noteTypePerTempo: 4,
-      speed: 80
-    }),
-    note(1, 8, -2),
-    note(1, 8, -2),
-    note(1, 16, -2, {
-      ascent: 1
-    }),
-    note(2, 16, -2),
-    note(3, 16, -2),
-    note(4, 16, -2),
-    measureStart(1, 4, 2,{
-      slur: [0]
-    }),
-    note(4, 4, 2, {
-      slur: [2]
-    }),
-    note(3, 4, 0, {
-      apg: [{
-        keyTxt: 2,
-        octave: 1,
-        ascent: 0
-      }
-    ]
-    }),
-    note(1, 4),
-    measureStart(6, 4),
-    note(7, 12, 0, {
-      triplet: [3]
-    }),
-    note(1, 12),
-    note(3, 12, 0, {
-      triplet: [3]
-    }),
-    note(5, 12),
-    note(7, 12),
-    note(2, 12),
-    note(4, 4),
-    measureStart(6, 4),
-    note(7, 4),
-    note(2, 4),
-    note(3, 4),
-    measureStart(5, 4),
-    note(6, 4),
-    note(7, 4),
-    note(3, 4),
-    measureStart(2, 4),
-    note(4, 4),
-    note(1, 4),
-    note(1, 4),
-    measureStart(1, 4),
-    note(3, 4),
-    note(2, 4),
-    note(1, 4, 0, {
-      tie: [1]
-    }),
-    measureStart(1, 4, 0, {
-      tie: [1] 
-    }),
-    note(7, 4),
-    note(2, 4),
-    note(1, 4, 0),
     {
-      skipMeasures: 10,
-      measureStart: true
+      "keyTxt": 1,
+      "noteType": 4,
+      "measureStart": true,
+      "tempoPerMeasure": 4,
+      "noteTypePerTempo": 4,
+      "speed": 80,
+      "tuneAscent": {   // 2 sharp = D
+        "sign": 1,      // 0 = flat(♭), 1 = sharp(#)
+        "num": 1        // how many sign (1 sharp = G, 2 sharp = D, 3 sharp = A etc.)
+      }
     },
+    {
+      "keyTxt": 2,
+      "noteType": 4
+    },
+    {
+      "keyTxt": 3,
+      "noteType": 4
+    },
+    {
+      "keyTxt": 4,
+      "noteType": 4
+    },
+    {
+      "keyTxt": 5,
+      "noteType": 4,
+      "measureStart": true
+    },
+    {
+      "keyTxt": 6,
+      "noteType": 4
+    },
+    {
+      "keyTxt": 7,
+      "noteType": 4
+    },
+    {
+      "keyTxt": 1,
+      "noteType": 4,
+      "octave": 1
+    },
+    {
+      "keyTxt": 2,
+      "noteType": 4,
+      "measureStart": true,
+      "octave": 1
+    },
+    {
+      "keyTxt": 3,
+      "noteType": 4,
+      "octave": 1
+    },
+    {
+      "keyTxt": 4,
+      "noteType": 4,
+      "octave": 1
+    },
+    {
+      "keyTxt": 5,
+      "noteType": 4,
+      "octave": 1
+    },
+    {
+      "keyTxt": 6,
+      "noteType": 4,
+      "measureStart": true,
+      "octave": 1
+    },
+    {
+      "keyTxt": 7,
+      "noteType": 4,
+      "octave": 1
+    },
+    {
+      "keyTxt": 1,
+      "noteType": 4,
+      "octave": 2
+    },
+    {
+      "keyTxt": 2,
+      "noteType": 4,
+      "octave": 2
+    }
   ]
 }
 const App = () => {
   return <div className="App">
-    <Score 
+    <NumStaffScore 
       score={score}
       pageWidth={(210 - 10)*5}
       pageHeight={(297 - 10)*5}
       maxLineWeight={7.81}
       size={5} 
       renderHeader={false}
-      renderPageID={true} 
+      renderPageID={true}
     />
   </div>
 }
 
 export default App
+
